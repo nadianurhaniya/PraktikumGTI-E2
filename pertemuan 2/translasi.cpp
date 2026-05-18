119e37d04b419a2d55905b23efb59993b2c5fa9f
Nadia Azura Nurhaniya <nadianurhaniya@gmail.com>
2026-05-18 12:19:18 +0700

    Add files via upload

---
+++ pertemuan 2/translasi.cpp
@@ -0,0 +1,23 @@
+//#include <GL/glut.h>
+//#include <GL/gl.h>
+//#include <math.h>
+//
+//void SegiEmpat(void) {
+//	glClear(GL_COLOR_BUFFER_BIT);
+//	glTranslatef(-0.50, 0.50, 0.0);
+//	glColor3f(1.0f, 0.0f, 0.0f);
+//	glRectf(-0.18, 0.18, 0.18, -0.18);
+//	glFlush();
+//}
+//
+//int main(int argc, char* argv[])
+//{
+//	glutInit(&argc, argv);
+//	glutInitWindowSize(640, 480);
+//	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+//	glutCreateWindow("Simple");
+//	glutDisplayFunc(SegiEmpat);
+//	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+//	glutMainLoop();
+//	return 0;
+//}
